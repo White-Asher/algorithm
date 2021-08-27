8136bccbac98891f50f3c0e6cf29ffa4fca5d57e
WhiteHairHAN <protoss1207@gmail.com>
2021-08-27 22:22:48 +0900

    Upload study files 08/27

--- Baekjoon_OJ/Greedy/5585.py
+++ Baekjoon_OJ/Greedy/5585.py
@@ -1,1 +1,9 @@
-money = int(input())
+price = int(input())
+money = 1000-price
+changeMoney = [500,100,50,10,5,1]
+count = 0
+for i in range(0,len(changeMoney)):
+    count += money // changeMoney[i]
+    money %= changeMoney[i]
+
+print(count)
