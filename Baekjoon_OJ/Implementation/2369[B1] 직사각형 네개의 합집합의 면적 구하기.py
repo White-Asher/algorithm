720d9707074e56fc431bad58560997bde3b86757
White-Asher <protoss1207@gmail.com>
2022-04-20 13:43:56 +0900

    TIL-220420

--- Baekjoon_OJ/Implementation/2369[B1] 직사각형 네개의 합집합의 면적 구하기.py
+++
@@ -1,4 +0,0 @@
-a = []
-for i in range(4):
-    a.append(list(map(int,input().split())))
-    
---
+++ SWEA-IM/2669[B1] 직사각형 네개의 합집합의 면적 구하기 [X].py
@@ -0,0 +1,27 @@
+board = [[0 for _ in range(101)] for _ in range(101)]
+# (x 시작, y시작, x끝, y끝)
+
+for _ in range(4):
+    x1, y1, x2, y2 = map(int, input().split())
+    for i in range(x1, x2):
+        for j in range(y1, y2):
+            board[i][j] = 1
+            
+result = 0   
+         
+for k in range(101):
+    for l in range(101):
+        if board[k][l] == 1:
+            result += 1
+            
+print(result)
+
+# 다른사람의 코드
+check_cor=[]
+for i in range(4):
+    inp = list(map(int, input().split()))
+    for i in range(inp[0], inp[2]):
+        for j in range(inp[1], inp[3]):
+            check_cor.append((i,j))
+
+print(len(set(check_cor)))
