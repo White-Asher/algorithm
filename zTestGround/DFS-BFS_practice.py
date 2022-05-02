bef8e0dbe4e341d1c9d759e5ddb9ef6896afa947
White-Asher <protoss1207@gmail.com>
2022-05-02 15:47:56 +0900

    Practice

--- zTestGround/DFS-BFS_practice.py
+++ zTestGround/DFS-BFS_practice.py
@@ -1,7 +1,8 @@
 from collections import deque
-n,m,v = map(int,input().split())
 
-graph = [[] for _ in range(n)]
+n, m, v = map(int,input().split())
+graph = [[] for i in range(n+1)]
+
 for i in range(m):
     a,b = map(int,input().split())
     graph[a].append(b)
@@ -9,27 +10,27 @@ for i in range(m):
     graph[a].sort()
     graph[b].sort()
     
-def dfs (graph, v, visited):
+def dfs(graph, v, visited):
     visited[v] = True
     print(v, end=' ')
     for j in graph[v]:
         if not visited[j]:
             dfs(graph, j, visited)
-            
-def bfs (graph, start, visited):
-    queue = deque([start])
-    visited[start] = True
+
+def bfs(graph,visited,started):
+    queue = deque([started])
+    visited[started] = True
+    
     while queue:
         v = queue.popleft()
-        print(v, end=' ')
+        print(v, end= ' ')
         for k in graph[v]:
             if not visited[k]:
                 queue.append(k)
-                visited[i] = True
-    
+                visited[k] = True
 
 visited = [False] * (n+1)
 dfs(graph, v, visited)
 print("")
 visited = [False] * (n+1)
-bfs(graph, v, visited)
+bfs(graph, visited, v)
--- zTestGround/bfs_miro.py
+++ zTestGround/bfs_miro.py
@@ -1,14 +1,16 @@
 from collections import deque
-n, m = map(int, input().split())
+
+n,m = map(int,input().split())
+
+dx = [0,0,-1,1]
+dy = [1,-1,0,0]
 
 graph = []
 for i in range(n):
-    graph.append(list(map(int, input())))
+    graph.append(list(map(int,input())))
+print(graph)
 
-dx = [ -1,1,0,0]# 좌 우
-dy = [0,0,-1,1]# 하 상
-
-def bfs (x,y):
+def bfs(x,y):
     queue = deque()
     queue.append((x,y))
     
@@ -19,12 +21,16 @@ def bfs (x,y):
             nx = x + dx[i]
             ny = y + dy[i]
             
-            if nx<0 or nx>=n or ny<0 or ny>=y:
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
+
+            if graph[nx][ny] == 0:
+                continue
+            
             if graph[nx][ny] == 1:
-                graph[nx][ny] =  graph[x][y] + 1
-                queue.append(nx,ny)
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx,ny))
+        
     return graph[n-1][m-1]
 
-print(bfs(0,0))            
-    
+print(bfs(0,0))
