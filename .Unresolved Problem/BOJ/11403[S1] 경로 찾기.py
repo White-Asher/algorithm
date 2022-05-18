126986d778ca603df5a9b3d76c625574f48d56db
White-Asher <jaeyoon.han97@gmail.com>
2022-05-19 00:53:59 +0900

    Solve: BOJ-11403 경로 찾기

--- .Unresolved Problem/BOJ/11403[S1] 경로 찾기.py
+++ .Unresolved Problem/BOJ/11403[S1] 경로 찾기.py
@@ -1,21 +1,61 @@
+# BFS
 from collections import deque
 
 n = int(input())
-
-graph =[[]]
-for _ in range(n):
-    graph.append(list(map(int, str(input().rstrip()))))
-
-visited = [False] * (n+1)
+graph = [list(map(int, input().split())) for _ in range(n)]
+visited = [[0]*n for _ in range(n)]
 
 def bfs(x):
-    queue = deque([x])
-    visited[x] = True
+    queue = deque()
+    queue.append(x)
+    check = [0 for _ in range(n)]
 
     while queue:
         q = queue.popleft()
 
-        for i in graph[q]:
-            if not visited[i]:
+        for i in range(n):
+            if check[i] == 0 and graph[q][i] == 1:
                 queue.append(i)
-                visited[i] = True
+                check[i] = 1
+                visited[x][i] = 1
+
+for i in range(0, n):
+    bfs(i)
+
+for i in visited:
+    print(*i)
+
+# DFS
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+visited = [0 for _ in range(n)]
+
+def dfs(x):
+    for i in range(n):
+        if graph[x][i] == 1 and visited[i] == 0:
+            visited[i] = 1
+            dfs(i)
+
+visited = [0 for _ in range(n)]
+for i in range(n):
+    dfs(i)
+    for j in range(n):
+        if visited[j] == 1:
+            print(1, end=' ')
+        else:
+            print(0, end=' ')
+    print()
+    visited = [0 for _ in range(n)]
+
+# 플로이드-와샬 알고리즘을 이용한 풀이
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            if graph[j][i] and graph[i][k]:
+                graph[j][k] = 1
+
+for g in graph:
+    print(*g)
