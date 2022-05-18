01495314677d05f2f4131cc42f09d32e9139b920
White-Asher <jaeyoon.han97@gmail.com>
2022-05-18 11:09:36 +0900

    Rename file name

--- Baekjoon_OJ/DataStructure/1620[S4] 나는야 포켓몬 마스터 이다솜.py
+++ Baekjoon_OJ/DataStructure/1620[S4] 나는야 포켓몬 마스터 이다솜.py
@@ -1,25 +1,25 @@
-# 시간초과(오답)
-import sys
-input = sys.stdin.readline
-n, m = map(int, input().split())
-poke = {}
-for i in range(n):
-    key = i + 1
-    value = input().rstrip()
-    poke[key] = value
-
-for i in range(m):
-    quest = input().rstrip()
-    if quest.isdigit():
-        for key, value in poke.items(): # 문제의 코드
-            if key == int(quest):
-                print(value)
-    else:
-        for key, value in poke.items(): # 문제의 코드
-            if str(value) == str(quest):
-                print(int(key))
-
-# 통과
+# # 시간초과(오답)
+# import sys
+# input = sys.stdin.readline
+# n, m = map(int, input().split())
+# poke = {}
+# for i in range(n):
+#     key = i + 1
+#     value = input().rstrip()
+#     poke[key] = value
+#
+# for i in range(m):
+#     quest = input().rstrip()
+#     if quest.isdigit():
+#         for key, value in poke.items(): # 문제의 코드
+#             if key == int(quest):
+#                 print(value)
+#     else:
+#         for key, value in poke.items(): # 문제의 코드
+#             if str(value) == str(quest):
+#                 print(int(key))
+#
+# # 통과
 import sys
 input = sys.stdin.readline
 n, m = map(int, input().split())
