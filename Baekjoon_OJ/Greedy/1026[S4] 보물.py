fde56fecd6909ffbafecd55aacfe18f748f8eeb0
White-Asher <jaeyoon.han97@gmail.com>
2022-05-22 21:17:26 +0900

    Style: BOJ-1026

--- Baekjoon_OJ/Greedy/1026[S4] 보물.py
+++ Baekjoon_OJ/Greedy/1026[S4] 보물.py
@@ -1,18 +1,16 @@
 # 답으로 인정되지만 문제 조건을 위반한 풀이
-# n = int(input())
-# a = list(map(int,input().split()))
-# b = list(map(int,input().split()))
-# a.sort(reverse=True)
-# b.sort()
-# answer = 0
-# for i in range(n):
-#     temp = a[i] * b[i]
-#     answer += temp
-    
-# print(answer)
+n = int(input())
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+a.sort(reverse=True)
+b.sort()
+answer = 0
+for i in range(n):
+    temp = a[i] * b[i]
+    answer += temp
+print(answer)
 
 # 문제 조건을 만족한 풀이
-
 n = int(input())
 a = list(map(int,input().split()))
 b = list(map(int,input().split()))
