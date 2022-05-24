d1ecbea30b30bf98cabf489d6a8a8bae48b87542
White-Asher <jaeyoon.han97@gmail.com>
2022-05-24 23:58:14 +0900

    Unsolve

---
+++ .Unresolved Problem/SWEA/1234[D3] 문제해결 기본 10일차 - 비밀번호.py
@@ -0,0 +1,16 @@
+for tc in range(1, 11):
+    stack = []
+    n, nums = input().split()
+    nums = list(nums)
+
+    for i in nums:
+        if not stack:
+            stack.append(i)
+        else:
+            if stack[-1] == i:
+                stack.pop()
+            else:
+                stack.append(i)
+
+    print("#{} {}".format(tc, ''.join(stack)))
+
---
+++ .Unresolved Problem/SWEA/9700[D3] USB 꽂기의 미스터리.py
@@ -0,0 +1,3 @@
+t = int(input())
+for tc in range(t, t+1):
+    p, q = map(input().split())
