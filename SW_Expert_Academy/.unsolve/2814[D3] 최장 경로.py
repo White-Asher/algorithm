cbf1aeb0f3f5b87e077a78f1c07efe7263bb8fce
White-Asher <jaeyoon.han97@gmail.com>
2022-05-28 22:55:22 +0900

    Solve: SWEA-2814 최장 경로

---
+++ SW_Expert_Academy/.unsolve/2814[D3] 최장 경로.py
@@ -0,0 +1,23 @@
+def dfs(idx,cnt):
+    global maxVal
+    visit[idx] = True
+    if cnt > maxVal:
+        maxVal = cnt
+    for i in s[idx]:
+        if not visit[i]:
+            dfs(i,cnt+1)
+            visit[i] = 0
+t = int(input())
+for tc in range(1, t+1):
+    n,m = map(int,input().split())
+    s=[[] for i in range(n+1)]
+    for i in range(m):
+        x,y = map(int,input().split())
+        s[x].append(y)
+        s[y].append(x)
+    maxVal = 0
+
+    for i in range(1,n+1):
+        visit = [0] * (n+1)
+        dfs(i,1)
+    print('#%d %d' % (tc, maxVal))
