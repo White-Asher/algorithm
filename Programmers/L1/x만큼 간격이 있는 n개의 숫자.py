ed5bdbd07d1e1aeaf10835ff8408948de8b062b8
White-Asher <jaeyoon.han97@gmail.com>
2022-06-02 22:16:51 +0900

    Solve: PRO-L1 x만큼 간격이 있는 n개의 숫자

---
+++ Programmers/L1/x만큼 간격이 있는 n개의 숫자.py
@@ -0,0 +1,12 @@
+def solution(x, n):
+    answer = []
+    for i in range(1, n+1):
+        answer.append(x*i)
+    return answer
+
+'''
+def number_generator(x, n):
+    return [i * x + x for i in range(n)]
+print(number_generator(2, 5))
+
+'''
