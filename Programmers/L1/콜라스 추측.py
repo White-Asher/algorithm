0e560820b703af66c55f9febb2df21de5a90ff81
White-Asher <jaeyoon.han97@gmail.com>
2022-06-02 22:18:38 +0900

    Solve: PRO-L1 클라스 추측

---
+++ Programmers/L1/콜라스 추측.py
@@ -0,0 +1,16 @@
+def solution(n):
+    cnt = 0
+    while cnt <= 500:
+        if n == 1:
+            break
+
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = n * 3 + 1
+        cnt += 1
+
+    if cnt > 500:
+        cnt = -1
+
+    return cnt
