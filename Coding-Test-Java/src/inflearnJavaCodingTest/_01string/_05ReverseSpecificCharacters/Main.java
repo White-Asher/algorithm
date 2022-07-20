d10fde858cbefdcb66975e90574a4f1bb673c9b8
White-Asher <jaeyoon.han97@gmail.com>
2022-07-21 08:49:49 +0900

    Study: reverseChar

--- Coding-Test-Java/src/inflearnJavaCodingTest/_01string/_05ReverseSpecificCharacters/Main.java
+++ Coding-Test-Java/src/inflearnJavaCodingTest/_01string/_05ReverseSpecificCharacters/Main.java
@@ -4,32 +4,29 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public Main() throws IOException {
+	public static void main(String[] args)  throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String input = br.readLine();
-		
-		
+		System.out.println(Main.solution(input));
 	}
-	
-	public static ArrayList<String> solution( String[] str) {
-		ArrayList<String> answer = new ArrayList<>();
-		for (String x : str) {
-			char[] s = x.toCharArray();
-			int lt = 0, rt = x.length() - 1;
-			while(lt<rt) {
+
+	public static String solution(String str) {
+		String answer;
+		char[] s = str.toCharArray();
+		int lt = 0, rt = str.length() - 1;
+		while (lt < rt) {
+			if (!Character.isAlphabetic(s[lt])) lt++;
+			else if (!Character.isAlphabetic(s[rt])) rt--;
+			else{
 				char tmp = s[lt];
-				if(Character.isLetterOrDigit(tmp)) {
-					s[lt] = s[rt];
-					s[rt] = tmp;
-				}
+				s[lt] = s[rt];
+				s[rt] = tmp;
 				lt++;
 				rt--;
 			}
-			String tmp = String.valueOf(s);
-			answer.add(tmp);
 		}
-		
-		
+		// s를 스트링화 시킨다.
+		answer = String.valueOf(s);
 		return answer;
 	}
 }
