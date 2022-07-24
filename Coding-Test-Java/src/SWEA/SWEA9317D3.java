c8fc7b86a635bc734c90c806d6675c9c08d40eae
White-Asher <jaeyoon.han97@gmail.com>
2022-07-24 23:40:30 +0900

    Solve: SWEA-9317

---
+++ Coding-Test-Java/src/SWEA/SWEA9317D3.java
@@ -0,0 +1,15 @@
+package SWEA;
+
+public class SWEA9317D3 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer();
+		int t = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= t; tc++) {
+			int ans = 0;
+
+			System.out.printf("#%d %d", tc, ans);
+		}
+	}
+}
