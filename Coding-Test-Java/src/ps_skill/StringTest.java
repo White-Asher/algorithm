92aa38f0b0d008b5b2a6ec01153b1e4bd8e8403e
White-Asher <jaeyoon.han97@gmail.com>
2022-07-24 23:38:25 +0900

    Study: java input practice

---
+++ Coding-Test-Java/src/ps_skill/StringTest.java
@@ -0,0 +1,10 @@
+package ps_skill;
+
+public class StringTest {
+	public static void main(String[] args) {
+		String str1 = "ABC";
+		str1 = str1.toLowerCase();
+		System.out.println(str1);
+		
+	}
+}
