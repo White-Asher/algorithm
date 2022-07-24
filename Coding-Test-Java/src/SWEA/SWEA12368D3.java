879f65e3217f9268969ec3ab8c8501e00056e87c
White-Asher <jaeyoon.han97@gmail.com>
2022-07-24 23:40:38 +0900

    Solve: SWEA-12368

---
+++ Coding-Test-Java/src/SWEA/SWEA12368D3.java
@@ -0,0 +1,29 @@
+package SWEA;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.FileInputStream;
+
+public class SWEA12368D3 {
+
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("src/SWEA/sample_input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+
+		for (int tc = 1; tc <= t; tc++) {
+			
+			int ans = 0;
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			ans = (x + y)%24;
+			
+			
+			System.out.printf("#%d %d\n", tc, ans);
+		}
+	}
+}
