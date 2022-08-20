476d8ef28ecf8916ec57c5800bdbae15c334a6ba
Han Jaeyoon <dkxmpower999@naver.com>
2022-08-20 11:50:22 +0900

    Solve: BOJ-15683

---
+++ Coding-Test-Java/src/BOJ/_SSWtest/BOJ_15683.java
@@ -0,0 +1,141 @@
+package BOJ._SSWtest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BOJ_15683 {
+	
+	public static class CCTV{
+		int r;
+		int c;
+		int type;
+		
+		public CCTV(int r, int c, int type){
+			super();
+			this.r = r;
+			this.c = c;
+			this.type = type;
+		}
+	}
+	
+	static int N, M;
+	static StringTokenizer st;
+	static int[][] map;
+	static int[] cctvCase;
+	static List<CCTV> cctv;
+	static int[][] delta = {{-1,0},{0,1},{1,0},{0,-1}}; //상 우 하 좌
+	static int minBlindArea;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		init();
+		duplicatePermutation(0);
+		System.out.println(minBlindArea);
+	}
+	
+	public static void init() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new int[N][M];
+		cctv = new ArrayList<>();
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				int temp = Integer.parseInt(st.nextToken());
+				map[i][j] = temp;
+				if(temp >= 1 && temp <= 5) {
+					cctv.add(new CCTV(i,j,temp));
+				}
+			}
+		}
+		cctvCase = new int[cctv.size()];
+		minBlindArea = Integer.MAX_VALUE;
+	}
+	
+	public static void duplicatePermutation(int cnt) {
+		if(cnt == cctv.size()) {
+			searchBlindArea();
+			return;
+		}
+		for (int i = 0; i < 4; i++) {
+			cctvCase[cnt] = i;
+			duplicatePermutation(cnt+1);
+		}
+	}
+	
+	
+	public static void searchBlindArea() {
+		int[][] tempMap = copyMap();
+		
+		for(int i = 0; i < cctv.size(); i++) {
+			int cr = cctv.get(i).r;
+			int cc = cctv.get(i).c;
+			int direction = cctvCase[i];
+
+			switch (cctv.get(i).type) {
+			case 1:
+				tempMap = checkBlindArea(direction, tempMap, cr, cc);
+				break;
+			case 2:
+				tempMap = checkBlindArea(direction, tempMap, cr, cc);
+				tempMap = checkBlindArea((direction+2) % 4, tempMap, cr, cc);
+				break;
+			case 3:
+				tempMap = checkBlindArea(direction, tempMap, cr, cc);
+				tempMap = checkBlindArea((direction+1) % 4, tempMap, cr, cc);
+				break;
+			case 4:
+				tempMap = checkBlindArea(direction, tempMap, cr, cc);
+				tempMap = checkBlindArea((direction+1) % 4, tempMap, cr, cc);
+				tempMap = checkBlindArea((direction+2) % 4, tempMap, cr, cc);
+				break;
+			case 5:
+				tempMap = checkBlindArea(direction, tempMap, cr, cc);
+				tempMap = checkBlindArea((direction+1) % 4, tempMap, cr, cc);
+				tempMap = checkBlindArea((direction+2) % 4, tempMap, cr, cc);
+				tempMap = checkBlindArea((direction+3) % 4, tempMap, cr, cc);
+				break;
+			}
+		}
+		countBlindArea(tempMap);
+	}
+	
+	public static int[][] copyMap() {
+		int[][] tempMap = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				tempMap[i][j] = map[i][j];
+			}
+		}
+		return tempMap;
+	}
+	
+	public static int[][] checkBlindArea(int dire, int[][] tempMap, int r, int c) {
+		while(true) {
+			r += delta[dire][0];
+			c += delta[dire][1];
+			if(r < 0 || r >= N || c < 0 || c >= M || tempMap[r][c] == 6) break;
+			if(tempMap[r][c] != 0) continue;
+			tempMap[r][c] = 9;
+			
+		}
+		return tempMap;
+	}
+	
+	public static void countBlindArea(int[][] tempMap) {
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if(tempMap[i][j] == 0) cnt++;
+			}
+		}
+		minBlindArea = Math.min(minBlindArea, cnt);
+	}
+}
---
+++ Coding-Test-Java/src/BOJ/_SSWtest/BOJ_15683_감시.java
@@ -0,0 +1,90 @@
+package BOJ._SSWtest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BOJ_15683_감시 {
+	static class CCTV{
+		int r,c,d;
+		CCTV(int r, int c, int d){
+			this.r = r;
+			this.c = c;
+			this.d = d;
+		}
+	}
+	
+	static int N,M, ans = Integer.MAX_VALUE;
+	static List<CCTV> cctvList;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		
+		int[][] map = new int[N][M];
+		cctvList = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				map[i][j] = sc.nextInt();
+				if(map[i][j] >= 1 && map[i][j] <=5) {
+					cctvList.add(new CCTV(i,j,map[i][j]));
+				}
+			}
+		}
+		solve(0,map);
+		System.out.println(ans);
+	}
+	
+	// 90도 형태로 돌리기 상 -> 우 -> 하 -> 좌
+	static int[] dr = {-1,0,1,0}; 
+	static int[] dc = {0,1,0,-1};
+	static int[][] dir = { // CCTV 번호에 해당하는 기본적으로 설정된 방향
+			{}, {1}, {1,3}, {0,1}, {0,1,3} ,{0,1,2,3}
+	};
+	// 돌리는 횟수에 대한 배열
+	static int[] dCnt = {0,4,2,4,4,1};
+	
+	// cctv하나가 해야할 일 정의
+	public static void solve(int idx, int[][] map) {
+		if(idx == cctvList.size()) {
+			int cnt = 0;
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < M; j++) {
+					 if(map[i][j] == 0) cnt++;
+				}
+			}
+			ans = Math.min(ans, cnt);
+			return;
+		}
+		CCTV cctv = cctvList.get(idx);
+		for (int d = 0; d < dCnt[cctv.d]; d++) { // 회전하기 위한 반복
+			int[][] copyMap = mapCopy(map);
+			for (int i = 0; i < dir[cctv.d].length ; i++) { // CCTV의 종류가 가지고 있는 방향으로 체크
+				int nd = (dir[cctv.d][i] + d) %4;
+				int nr = cctv.r;
+				int nc = cctv.c;
+				
+				while(true) {
+					nr += dr[nd];
+					nc += dc[nd];
+					if(nr <0 || nr >= N || nc < 0 || nc >= M) break; // 범위 밖으로 나간 경우
+					if(copyMap[nr][nc] == 6) break; // 벽을 만난 경우
+					copyMap[nr][nc] = 9;
+				}
+			}
+			solve(idx+1, copyMap);
+		}
+	}
+	
+	static int[][] mapCopy(int[][] map){
+		int[][] copy = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				copy[i] = map[i].clone();
+			}
+		}
+		return copy;
+	}
+
+}
