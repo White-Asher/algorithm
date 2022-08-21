78d0c55d394dfc6b65df0bf484549a767f74cdaf
Han Jaeyoon <dkxmpower999@naver.com>
2022-08-21 22:02:28 +0900

    Solve: BOJ-17281

---
+++ Coding-Test-Java/src/BOJ/_SSWtest/BOJ_17281.java
@@ -0,0 +1,148 @@
+package BOJ._SSWtest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_17281 {
+	static int N;
+	static final int BASEBALL_PLAYER_NUMBER = 9;
+	static final int OUT_CONDITION = 3;
+	static int[] playerTempOrder = new int[BASEBALL_PLAYER_NUMBER-1];
+	static int[] playerOrder = new int[BASEBALL_PLAYER_NUMBER];
+	static int[] inputs = {1,2,3,4,5,6,7,8};
+	static int[][] playerScoreArray;
+	static int maxScore = 0;
+	
+	static int TotalCnt;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		inputData();
+		permutation(0,0);
+		System.out.println(maxScore);
+	}
+	
+	public static void playGame() {
+		
+		int playerStart = 0;
+		int caseScore = 0;
+		int[] order = playerOrder;
+		
+		for(int inning = 0; inning < N; inning++) {
+			
+			int outCount = 0;
+			boolean[] baseCheck = new boolean[4];
+			
+			outer: while (true) {
+	
+				for (int player = playerStart; player < BASEBALL_PLAYER_NUMBER; player++) {
+					
+					int playerScore = playerScoreArray[inning][order[player]];
+
+					switch (playerScore) {
+					
+					case 0:
+						outCount++;
+						break;
+					case 1:
+						for (int base = 3; base >= 1; base--) {
+                            if (baseCheck[base]) {
+                                if (base == 3) { 
+                                	baseCheck[base] = false; 
+                                	caseScore++; 
+                                    continue;
+                                }
+                                baseCheck[base] = false;
+                                baseCheck[base + 1] = true;
+                            }
+                        }
+						baseCheck[1] = true; 
+                        break;
+						
+					case 2:
+						for (int base = 3; base >= 1; base--) {
+                            if (baseCheck[base]) {
+                                if (base == 3 || base == 2) {
+                                	baseCheck[base] = false; 
+                                	caseScore++; 
+                                    continue;
+                                }
+                                baseCheck[base] = false;
+                                baseCheck[base + 2] = true;
+                            }
+                        }
+						baseCheck[2] = true; 
+                        break;
+						
+					case 3:
+						for (int base = 3; base >= 1; base--) {
+                            if (baseCheck[base]) { 
+                            	baseCheck[base] = false;
+                            	caseScore++; 
+                            }
+                        }
+ 
+						baseCheck[3] = true; 
+                        break;
+						
+					case 4:
+						for (int base = 1; base <= 3; base++) {
+                            if (baseCheck[base]) {
+                            	baseCheck[base] = false;
+                            	caseScore++; 
+                            }
+                        }
+						caseScore++; 
+                        break;
+					}
+		
+					if (outCount == OUT_CONDITION) {
+						playerStart = player + 1;
+						if(playerStart == BASEBALL_PLAYER_NUMBER) player = 0;
+						break outer;
+					}
+					
+				}
+				playerStart = 0;
+			}
+		}
+		
+		maxScore = Math.max(maxScore, caseScore);
+	}
+	
+	
+	public static void permutation(int cnt, int flag) {
+		if(cnt == BASEBALL_PLAYER_NUMBER-1) {
+			createOrder();
+			playGame();
+			return;
+		}
+
+		for(int i = 0; i < BASEBALL_PLAYER_NUMBER- 1; i++) {
+			if( (flag & 1 << i) != 0) continue;
+			playerTempOrder[cnt] = inputs[i];
+			permutation(cnt+1, flag | 1 << i);
+		}
+	}
+	
+	public static void createOrder() {
+		playerOrder[3] = 0;
+		for (int i = 0; i < 3; i++) playerOrder[i] = playerTempOrder[i];
+		for (int i = 3; i < 8; i++) playerOrder[i+1] = playerTempOrder[i];
+	}
+	
+	public static void inputData() throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		playerScoreArray = new int[N][BASEBALL_PLAYER_NUMBER];
+		
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < BASEBALL_PLAYER_NUMBER; j++) {
+				playerScoreArray[i][j] = Integer.parseInt(st.nextToken());				
+			}
+		}
+	}
+}
---
+++ Coding-Test-Java/src/BOJ/_SSWtest/BOJ_17281_CodeRefactoring.java
@@ -0,0 +1,86 @@
+package BOJ._SSWtest;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_17281_CodeRefactoring {
+	static int[] order;
+	static boolean[] visit;
+	static int n;
+	static int[][] map;
+	static int answer = Integer.MIN_VALUE;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		map = new int[n][9];
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			for (int j = 0; j < 9; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		order = new int[9];
+		order[3] = 0;
+		visit = new boolean[9];
+		visit[0] = true;
+		select(0);
+
+		System.out.println(answer);
+	}
+
+	private static void select(int end) {
+		if (end == 9) { // 하나의 팀 구성이 완료되는 때
+			int score = play(); // 한번 정해지면 n이닝이 끝날때까지 안바뀐다.
+			answer = Math.max(answer, score);
+			return;
+		}
+		if (end == 3) {
+			select(end + 1);
+			return;
+		}
+		for (int i = 0; i < 9; i++) {
+			if (visit[i] == false) {
+				visit[i] = true;
+				order[end] = i;
+				select(end + 1);
+				visit[i] = false;
+			}
+		}
+
+	}
+
+	private static int play() {
+		int score = 0;
+		int cur = 0;
+		for (int i = 0; i < n; i++) { // n회의 이닝에 대해서
+			int base = 0;
+			int goal = 1<<4;
+			int out = 0; // 아웃 횟수
+			while (out < 3) {
+				int r = map[i][order[cur % 9]];
+
+				if (r == 0) {
+					out++;
+				} 
+				else {
+					base=(base|1);
+					for(int j=0;j<r;j++) {
+						base = base<<1;
+						if((base&goal)>0)
+							score++;
+					}
+				}
+
+				cur++;
+			}
+
+		}
+		return score;
+	}
+
+}
