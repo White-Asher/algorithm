1b9d12f74f8c20cbdd00b2876c321f3f28ff6fcb
White-Asher <jaeyoon.han97@gmail.com>
2023-07-21 22:44:42 +0900

    Update BOJ_2023_제곱ㄴㄴ수.java

--- Coding-Test-Java/src/BOJ/Math/BOJ_1016_G1_제곱ㄴㄴ수/BOJ_2023_제곱ㄴㄴ수.java
+++ Coding-Test-Java/src/BOJ/Math/BOJ_1016_G1_제곱ㄴㄴ수/BOJ_2023_제곱ㄴㄴ수.java
@@ -8,14 +8,33 @@ public class BOJ_2023_제곱ㄴㄴ수 {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
-        int min = Integer.parseInt(st.nextToken());
-        int max = Integer.parseInt(st.nextToken());
+        long min = Long.parseLong(st.nextToken());
+        long max = Long.parseLong(st.nextToken());
 
-        int ans = 0;
+        boolean[] flag = new boolean[(int) (max - min + 1)];
+        List<Long> modNum = new ArrayList<>();
+        for (long i = 2; i <= Math.sqrt(max); i++) {
+            modNum.add(i*i);
+        }
 
+        for (int i = 0; i < modNum.size(); i++) {
+            long mod = modNum.get(i);
+            double target = (double) min / (double) mod;
+            long sNum = (long) Math.ceil(target) * (long) mod - min;
+
+            for (long j = sNum; j < flag.length; j += mod) {
+                flag[(int) j] = true;
+            }
+
+        }
+        int cnt = 0;
+        for (int i = 0; i < flag.length; i++) {
+            if(!flag[i]) {
+                cnt++;
+            }
+        }
+
+        System.out.println(cnt);
     }
 }
 
-// 소수 제곱 전부 제외
-// 제곱수 전부 제외
-// 제곱수 전부 제외 
