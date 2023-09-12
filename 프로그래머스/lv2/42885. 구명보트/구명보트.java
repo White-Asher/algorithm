5f29faab06e9d221b6e9a1df9eebb82217dd75e6
Han Jaeyoon <dkxmpower999@naver.com>
2023-09-13 02:27:04 +0900

    [level 2] Title: 구명보트, Time: 12.00 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        
+        int s = 0;
+        int e = people.length-1;
+        
+        while (s <= e){
+            if(people[s] + people[e] <= limit) {
+                s++;
+                e--;
+            } else {
+                e--;
+            }
+            answer++;
+        }
+        
+        return answer;
+    }
+}
