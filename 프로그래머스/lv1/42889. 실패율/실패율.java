59aca557ef59a31f302005e476420149baa4c566
Han Jaeyoon <dkxmpower999@naver.com>
2023-09-20 21:31:39 +0900

    [level 1] Title: 실패율, Time: 0.53 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.java
+++ 프로그래머스/lv1/42889. 실패율/실패율.java
@@ -1,5 +1,58 @@
-
 // note
+import java.util.*;
+
+// best 
+class Solution {
+    public int[] solution(int N, int[] lastStages) {
+        int nPlayers = lastStages.length;
+        int[] nStagePlayers = new int[N + 2];
+        for (int stage : lastStages) {
+            nStagePlayers[stage] += 1;
+        }
+
+        int remainingPlayers = nPlayers;
+        List<Stage> stages = new ArrayList<>();
+        for (int id = 1 ; id <= N; id++) {
+            double failure = (double) nStagePlayers[id] / remainingPlayers;
+            remainingPlayers -= nStagePlayers[id];
+
+            Stage s = new Stage(id, failure);
+            stages.add(s);
+        }
+        Collections.sort(stages, Collections.reverseOrder());
+
+        int[] answer = new int[N];
+        for (int i = 0; i < N; i++) {
+            answer[i] = stages.get(i).id;
+        }
+        return answer;
+    }
+
+    class Stage implements Comparable<Stage> {
+        public int id;
+        public double failure;
+
+        public Stage(int id_, double failure_) {
+            id = id_;
+            failure = failure_;
+        }
+
+        @Override
+        public int compareTo(Stage o) {
+            if (failure < o.failure ) {
+                return -1;
+            }
+            if (failure > o.failure ) {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
+
+
+// 시도한 solution
+/*
 import java.util.*;
 
 class Solution {
@@ -40,8 +93,8 @@ class Solution {
 			// System.out.println("key : " + key + " , " + "value : " + map.get(key));
             answer[idx++] = key;
 		}
-        
-
+    
         return answer;
     }
 } 
+*/
