bce692e7653b507b4f1cd9b5a13143d749cd7d25
Han Jaeyoon <dkxmpower999@naver.com>
2023-09-24 09:43:10 +0900

    [level 2] Title: 가장 큰 수, Time: 2.29 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -3,48 +3,24 @@ import java.io.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String[] strNumbers = new String[numbers.length];
-        
+        StringBuilder sb = new StringBuilder();
+        List<Integer> list = new ArrayList<>();
         for(int i = 0; i < numbers.length; i++) {
-            strNumbers[i] = String.valueOf(numbers[i]);
-        }
-        // Array 정렬
-        Arrays.sort(strNumbers, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return (o2 + o1).compareTo(o1 + o2);
+            String[] str = String.valueOf(numbers[i]).split("");
+            for(int j = 0; j < str.length; j++) {
+                System.out.println(Integer.parseInt(str[j]));
+                list.add(Integer.parseInt(str[j]));
             }
-        });
-            
-        if(strNumbers[0].equals("0")) return "0";
+        }
         
-        return String.join("", strNumbers);
+        
+        Collections.sort(list, Collections.reverseOrder());
+        for(int i = 0; i < list.size(); i++) {
+            // System.out.println(list.get(i));
+            sb.append(list.get(i));
+        }
+        
+        
+        return sb.toString();
     }
 }
-
-/** 잘못 이해한 풀이 */
-// class Solution {
-//     public String solution(int[] numbers) {
-        
-//         List<Integer> list = new ArrayList<>();
-//         for(int i = 0; i < numbers.length; i++) {
-//             String[] str = String.valueOf(numbers[i]).split("");
-//             for(int j = 0; j < str.length; j++) {
-//                 // System.out.println(Integer.parseInt(str[j]));
-//                 list.add(Integer.parseInt(str[j]));
-//             }
-//         }
-        
-        
-//         Collections.sort(list);
-//         StringBuilder sb = new StringBuilder();
-//         String s = "";
-//         for(int i = list.size()-1; i >= 0; i--) {
-//             System.out.println(list.get(i));
-//             s = s + String.valueOf(list.get(i));
-//         }
-        
-        
-//         return s;
-//     }
-// }
