d198b6ca6f6c7943dead3350b2d428909e8a00bb
Han Jaeyoon <dkxmpower999@naver.com>
2023-10-08 01:05:12 +0900

    [level 2] Title: 소수 찾기, Time: 13.44 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.java
@@ -1,6 +1,55 @@
 import java.util.*;
 import java.io.*;
 
+class Solution{
+    static ArrayList<Integer> arr = new ArrayList<>();
+    static boolean[] check = new boolean[7];
+    
+    public int solution(String numbers) {
+        int answer = 0;
+        for(int i = 0; i < numbers.length(); i++) {
+            dfs(numbers, "", i + 1 );
+        }
+        
+        for(int i = 0; i < arr.size(); i++) {
+            if(isPrime(arr.get(i))) answer++;
+        }
+        return answer++;
+    }
+    
+    //백트래킹
+	static void dfs(String str, String temp, int m) {
+            if(temp.length() == m){
+                int num = Integer.parseInt(temp);
+                if(!arr.contains(num)){
+                    arr.add(num);
+                }
+            }
+        
+            for(int i = 0; i < str.length(); i++){
+                if(!check[i]) {
+                    check[i] = true;
+                    temp += str.charAt(i);
+                    dfs(str, temp, m);
+                    check[i] = false;
+                    temp = temp.substring(0, temp.length()-1);
+                }
+            }
+		
+	}
+	//소수 판별
+	static boolean isPrime(int n) {
+		if (n < 2) return false;
+		for(int i = 2; i * i <= n; i++) {
+			if(n % i == 0) return false;
+		} 
+		return true;
+	}
+}
+
+
+// 정답 (1차)
+/*
 class Solution {
     static String[] sarr;
     static String[] arr;
@@ -100,3 +149,4 @@ class Solution {
         return tar;
     }
 }
+*/
