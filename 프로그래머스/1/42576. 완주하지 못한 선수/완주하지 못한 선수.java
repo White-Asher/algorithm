0122bee9e66ec345c0a62829948b7bf28710f7fc
Han Jaeyoon <dkxmpower999@naver.com>
2023-10-11 23:53:01 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.51 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,6 +3,30 @@ import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String ans = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String s : participant) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+        for(String s : completion) {
+            map.put(s, map.get(s) - 1);
+        }
+        Iterator<Map.Entry<String, Integer>> iter = map.entrySet().iterator();
+        while(iter.hasNext()) {
+            Map.Entry<String, Integer> entry = iter.next();
+            if(entry.getValue() != 0) {
+                ans = entry.getKey();
+                break;
+            }
+        }
+        return ans;
+        
+    }
+}
+// sort
+/*
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String ans = "";
         Arrays.sort(participant);
         Arrays.sort(completion);
         
@@ -19,3 +43,4 @@ class Solution {
         return ans;
     }
 }
+*/
