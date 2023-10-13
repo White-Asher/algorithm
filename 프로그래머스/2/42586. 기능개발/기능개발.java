5d61df69bb875d602cce4207a59bf0b174840260
Han Jaeyoon <dkxmpower999@naver.com>
2023-10-13 20:54:34 +0900

    [level 2] Title: 기능개발, Time: 0.37 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public List<?> solution(int[] progresses, int[] speeds) {
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0; i < progresses.length; i++){
+            queue.add((int) (Math.ceil((100.0 - progresses[i]) / speeds[i])));
+        }
+        
+        List<Integer> list = new ArrayList<>();
+        
+        while(!queue.isEmpty()) {
+            int day = queue.poll();
+            int cnt = 1;
+            
+            while(!queue.isEmpty() && day >= queue.peek()) {
+                cnt++;
+                queue.poll();
+            }
+            list.add(cnt);
+        }
+        
+    
+        return list;
+    }
+}
