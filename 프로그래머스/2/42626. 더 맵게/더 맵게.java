27bfb0124bb5ba0a66c6d40292a2123fbc296bb4
Han Jaeyoon <dkxmpower999@naver.com>
2023-10-21 15:25:02 +0900

    [level 2] Title: 더 맵게, Time: 1619.73 ms, Memory: 127 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for (int s : scoville) {
+            pq.add(s);
+        }
+        
+        while (true) {
+            if (pq.size() <= 1 && pq.peek() < K) {
+                answer = -1;
+                break;
+            }
+            if (pq.peek() >= K) {
+                break;
+            }
+            int s1 = pq.poll();
+            int s2 = pq.poll();
+            pq.offer(s1 + s2*2);
+            answer++;
+            
+        }
+        return answer;
+    }
+}
