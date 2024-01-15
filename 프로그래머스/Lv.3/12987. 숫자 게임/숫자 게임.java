e15ca90b10a24115b023724a6a656e9ea0d3ffb9
Han Jaeyoon <dkxmpower999@naver.com>
2024-01-16 00:11:53 +0900

    [level Lv.3] Title: 숫자 게임, Time: 117.35 ms, Memory: 64.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int idx = 0;
+        for(int i = 0; i < A.length; i++) {
+            if(A[idx] >= B[i]) continue;
+            idx++;
+        }
+        return idx;
+    }
+}
