7b5f3d90d78c5cff7e0fa859b11907d33b4091eb
White-Asher <jaeyoon.han11@gmail.com>
2024-02-02 23:33:32 +0900

    Study: KMP

---
+++ Coding-Test-Java/src/_Playground/Theory/KMP/KMP_TEST.java
@@ -0,0 +1,66 @@
+package _Playground.Theory.KMP;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class KMP_TEST {
+    static StringTokenizer st;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String text = br.readLine();
+        String pattern = br.readLine();
+
+        KMP(text, pattern);
+    }
+
+    public static void KMP(String text, String pattern) {
+        int[] table = makeTable(pattern);
+        int tLength = text.length();
+        int pLength = pattern.length();
+        int idx = 0;
+        int cnt = 0;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < tLength; i++) {
+            // idx번 글자와 text의 타겟 글자와 불일치할 경우
+            // 현재 대응된 글자의 수를 table[idx-1]번으로 줄인다.
+            while (idx > 0 && text.charAt(i) != pattern.charAt(idx)) {
+                idx = table[idx - 1];
+            }
+            // 글자가 대응될 경우
+            if(text.charAt(i) == pattern.charAt(idx)) {
+                if(idx == pLength - 1) {
+                    idx = table[idx];
+                    System.out.println(1);
+                    return;
+                }
+                else {
+                    idx++;
+                }
+            }
+
+        }
+        System.out.println(0);
+        return;
+    }
+
+    private static int[] makeTable(String pattern) {
+        int pLength = pattern.length();
+        int[] table = new int[pLength];
+
+        int index = 0;
+        for (int i = 1; i < pLength; i++) {
+            // index > 0 => 문자열 매칭이 시작됨
+            // 연속으로 일치하지 않으면 index 값을 돌려주어 다시 매칭 시작
+            while(index > 0 && pattern.charAt(i) != pattern.charAt(index)) {
+                index = table[index - 1];
+            }
+
+            if(pattern.charAt(i) == pattern.charAt(index)) {
+                table[i] = ++index;
+            }
+        }
+        return table;
+    }
+}
