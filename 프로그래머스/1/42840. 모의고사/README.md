c885e3d41de7e061001d8d9e9ea23964e7383d2a
Han Jaeyoon <dkxmpower999@naver.com>
2024-05-07 22:55:01 +0900

    [level 1] Title: 모의고사, Time: 3.34 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 78.2 MB, 시간: 0.89 ms
+메모리: 75.5 MB, 시간: 3.34 ms
 
 ### 구분
 
@@ -12,7 +12,11 @@
 
 ### 채점결과
 
-Empty
+정확성: 100.0<br/>합계: 100.0 / 100.0
+
+### 제출 일자
+
+2024년 05월 07일 22:54:58
 
 ### 문제 설명
 
@@ -68,4 +72,4 @@ Empty
 </ul>
 
 
-> 출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
+> 출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -3,6 +3,42 @@ import java.io.*;
 
 class Solution {
     public int[] solution(int[] answers) {
+        // 수포자들의 패턴들
+        int[][] pattern = {
+            {1, 2, 3, 4, 5},
+            {2, 1, 2, 3, 2, 4, 2, 5},
+            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
+        
+        // 수포자들의 점수를 저장할 배열
+        int[] scores = new int[3];
+        
+        // 수포자들의 패턴과 정답과 일치하는 횟수 확인
+        for(int i = 0; i < answers.length; i++) {
+            for(int j = 0; j < pattern.length; j++) {
+                if(answers[i] == pattern[j][i % pattern[j].length]) {
+                    scores[j]++;
+                }
+            }
+        }
+        
+        // 가장 높은 점수 저장한다.
+        int maxScore = Arrays.stream(scores).max().getAsInt();
+        
+        // 가장 높은 점수를 가진 수포자들 번호 찾은 뒤 리스트에 적재
+        ArrayList<Integer> answer = new ArrayList<>();
+        for(int i = 0; i < scores.length; i++) {
+            if(scores[i] == maxScore) {
+                answer.add(i + 1);
+            }
+        }
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
+
+/*
+class Solution {
+    public int[] solution(int[] answers) {
         int[] aa = {1, 2, 3, 4, 5};
         int[] bb = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] cc = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
@@ -45,3 +81,4 @@ class Solution {
         return answer;
     }
 }
+*/
