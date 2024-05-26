cddff661c70b8da4e436bb35717d347bb7e01694
White-Asher <jaeyoon.han11@gmail.com>
2024-05-26 22:26:40 +0900

    Solve: BOJ_9655_S5_돌게임

---
+++ Coding-Test-Java/src/BOJ/Math/BOJ_9655_S5_돌게임/BOJ_9655_S5_돌게임.java
@@ -0,0 +1,10 @@
+package BOJ.Math.BOJ_9655_S5_돌게임;
+
+import java.util.*;
+
+public class BOJ_9655_S5_돌게임 {
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        System.out.print(s.nextInt()%2==0?"CY":"SK");
+    }
+}
