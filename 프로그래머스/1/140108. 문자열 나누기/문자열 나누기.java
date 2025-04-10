41f515fef93b1ba28e4a8bee9200ba4677da836e
Jae-yoon Han <dkxmpower999@naver.com>
2025-04-11 00:33:14 +0900

    [level 1] Title: 문자열 나누기, Time: 1.12 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,3 +1,17 @@
+/*
+ 제목 : 문자열나누기
+ 알고리즘 유형 : #문자열
+ 플랫폼 : #Programmers
+ 난이도 : L1
+ 문제번호 : 140108
+ 시간 : 30
+ 해결 : O
+ 저장 : O
+ 해결언어 : java
+ 문제링크 : https://school.programmers.co.kr/learn/courses/30/lessons/140108
+ 특이사항 : X
+ */
+
 import java.util.*;
 
 class Solution {
