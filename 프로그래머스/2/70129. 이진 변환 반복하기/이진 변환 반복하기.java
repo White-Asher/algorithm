150395a54939eda68504f2eedf79424d3aff67b0
Jae-yoon Han <dkxmpower999@naver.com>
2025-04-12 15:28:18 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 5.60 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,3 +1,17 @@
+/*
+제목 : 이진 변환 반복하기
+알고리즘 유형 : #문자열
+플랫폼 : #Programmers
+난이도 : L2
+문제번호 : 70129
+시간 : 20
+해결 : O
+저장 : O
+해결언어 : java
+문제링크 : https://school.programmers.co.kr/learn/courses/30/lessons/70129
+특이사항 : #[도서실습] 프로그래머스 코딩 테스트 문제 풀이 전략: 자바편
+*/
+
 class Solution {
     public int[] solution(String s) {
         int loopCnt = 0;
