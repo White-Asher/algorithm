7b303c454c1c889107c35a9d6b1dc2f2eb35bfe9
Jae-yoon Han <dkxmpower999@naver.com>
2025-04-12 20:42:05 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 25.46 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,3 +1,17 @@
+/*
+제목 : 쿼드압축 후 개수 세기
+알고리즘 유형 : #재귀 , #분할정복
+플랫폼 : #Programmers
+난이도 : L2
+문제번호 : 68936
+시간 : 30
+해결 : O
+저장 : O
+해결언어 : java
+문제링크 : https://school.programmers.co.kr/learn/courses/30/lessons/68936
+특이사항 : #[도서실습] 프로그래머스 코딩 테스트 문제 풀이 전략: 자바편
+*/
+
 class Solution {
     static int[] ans = new int[]{0,0};
     
