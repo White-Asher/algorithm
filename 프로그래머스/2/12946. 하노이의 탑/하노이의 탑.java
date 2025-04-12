24ef61f7844b406de1725654d7c69ec91c447fb3
Jae-yoon Han <dkxmpower999@naver.com>
2025-04-12 22:07:36 +0900

    [level 2] Title: 하노이의 탑, Time: 4.10 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,3 +1,17 @@
+/*
+제목 : 하노이의 탑
+알고리즘 유형 : #재귀 
+플랫폼 : #Programmers
+난이도 : L3
+문제번호 : 12946
+시간 : 60
+해결 : O
+저장 : O
+해결언어 : java
+문제링크 : https://school.programmers.co.kr/learn/courses/30/lessons/12946
+특이사항 : #[도서실습] 프로그래머스 코딩 테스트 문제 풀이 전략: 자바편
+*/
+
 import java.util.*;
 
 class Solution {
