64b07413220fe809b6211592f8e4ebb9ea92b8e9
Jae-yoon Han <dkxmpower999@naver.com>
2025-06-01 21:08:05 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.48 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,44 +1,38 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
-    static boolean[][] check;
-    static int[] dy = {-1,1,0,0};
-    static int[] dx = {0,0,-1,1};
+    static int[][] visit;
+    static int N, M;
+    
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
     
     public int solution(int[][] maps) {
-        check = new boolean[maps.length][maps[0].length];
-        
-        int answer = BFS(maps);
-        return answer;
+        N = maps.length;
+        M = maps[0].length;
+        visit = new int[N][M];
+        visit[0][0] = 1;
+        bfs(maps);
+        int ans = visit[N-1][M-1] == 0 ? -1 : visit[N-1][M-1];
+        return ans;
     }
     
-    public static int BFS(int[][] maps) {
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.add(new int[] {0,0,1});
-        check[0][0] = true;
+    public static void bfs(int[][] maps) {
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{0,0});
         
-        while(!queue.isEmpty()) {
-            int[] q = queue.poll();
-            int qy = q[0];
-            int qx = q[1];
-            int qc = q[2];
+        while(!q.isEmpty()) {
+            int[] cur = q.poll();
             
             for(int d = 0; d < 4; d++) {
-                int ny = qy + dy[d];
-                int nx = qx + dx[d];
-                if(ny >= maps.length || nx >= maps[0].length || ny < 0 || nx < 0) continue;
+                int ny = cur[0] + dy[d];
+                int nx = cur[1] + dx[d];
+                if(ny < 0 || nx < 0 || ny >= N || nx >= M) continue;
                 if(maps[ny][nx] == 0) continue;
-                if(check[ny][nx]) continue;
-                check[ny][nx] = true;
-                if(ny == maps.length -1 && nx == maps[0].length-1) {
-                    return qc+1;
-                }
-                queue.add(new int[] {ny, nx, qc+1});
-                
+                if(visit[ny][nx] != 0) continue;
+                visit[ny][nx] = visit[cur[0]][cur[1]] + 1;
+                q.add(new int[]{ny, nx});
             }
         }
-        
-        return -1;
     }
 }
